e2decb27e7918e3cf4e3348a4be65cf71c112fde
Samuel Rocha <samuelsilv.rocha@gmail.com>
2022-05-02 16:06:19 -0300

    Juncao em Video dos .jpegs

---
+++ rename.py
@@ -0,0 +1,10 @@
+import os
+
+path = 'video_em_frames/'
+
+arquivos = [x for x in os.listdir(path) if x.endswith('.jpeg')]
+print(arquivos[:10])
+
+for arquivo in arquivos:
+    novo  = arquivo.replace('p.jpeg','.jpeg').replace('i.jpeg','.jpeg')
+    os.rename(path+arquivo,path+novo)
